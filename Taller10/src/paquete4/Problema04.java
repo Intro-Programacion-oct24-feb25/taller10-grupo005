c360568ff05c8439fb946b50a7e7430474a94ce2
jhordycamacas <jhordycamacas9@gmail.com>
2024-12-19 12:21:05 -0500

    ejer4

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,54 @@
  */
 package paquete4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        double[][] datos = new double[2][2];
+        double valor;
+        int contador = 0;
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("Ingrese su valor para la posicion [%d][%d]: ",
+                        i,
+                        j);
+                valor = entrada.nextDouble();
+
+                if (valor > 1000 && valor < 1199) {
+                    datos[i][j] = valor;
+
+                } else {
+                    datos[i][j] = 10;
+                    contador = contador + 1;
+                }
+
+            }
+        }
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("Posicion [%d][%d] con valor: %.2f\n",
+                        i,
+                        j,
+                        datos[i][j]);
+
+            }
+
+        }
+
+        System.out.printf("El numero total de valores fuera de rango es: %d\n",
+                contador);
+
+    }
+
 }
